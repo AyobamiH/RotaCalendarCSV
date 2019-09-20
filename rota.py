b4ef189469c97ed34e11ff405a985fde00edec6a
LiamAEdwards <liam.a.edwards@protonmail.com>
2019-09-20 02:29:06 +0100

    Added a limit option to how much it generates

--- rota.py
+++ rota.py
@@ -1,9 +1,5 @@
 # Author - Liam Edwards
-
 from datetime import *
-
-f=open("rota.csv", "w+")
-
 
 def printingDates(count):
 # Prints 1 year of dates - Including time (Get rid of the time)
@@ -11,29 +7,33 @@ def printingDates(count):
     f.write(f"Work, {dateCount}, FALSE\n")
 
 
-
 # CSV Calendar Format
 # SUBJECT    START DATE   START TIME    END DATE   END TIME    ALL DAY EVENT   DESCRIPTION   PRIVATE
 
 def counting(count):
-    inc = 0
-    while inc < 4:
-        count += 1
-        printingDates(count)
-        inc += 1
+    i = 0
+    while i < shiftCount:
+        inc = 0
+        while inc < 4:
+            count += 1
+            printingDates(count)
+            inc += 1
 
-    inc = 0
-    while inc < 4:
-        count+= 1
-        inc += 1
-    inc = 0
-    while inc < 4:
-        count += 1
-        printingDates(count)
-        inc += 1
+        inc = 0
+        while inc < 4:
+            count+= 1
+            inc += 1
+        i += 1
+# Open a file
+with open("rota.csv", "w+") as f:
 
-print("WARNING - CURRENTLY NO WAY TO UNDO AFTER IMPORTING TO GOOGLE CALENDAR")
-print("Enter a number to offset the script. e.g. -1 = Today or 0 = Tomorrow")
-count = int(input())
-f.write('Subject, '+'Start Date, ' +'Private\n')
-counting(count)
+
+    print("WARNING - CURRENTLY NO WAY TO UNDO AFTER IMPORTING TO GOOGLE CALENDAR")
+    print("Enter a number to offset the script. e.g. -1 = Today or 0 = Tomorrow")
+    count = int(input())
+    print("How many 4 day shift period do you want to generate?")
+    shiftCount = int(input())
+
+    # Write to a file
+    f.write('Subject, '+'Start Date, ' +'Private\n')
+    counting(count)
